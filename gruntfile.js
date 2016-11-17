9caa25dae1496f84b384c5fb3f460f10c1c9145a
unsaidxpl <unsaidxpl@gmail.com>
2016-11-17 22:07:40 +0300

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sources: 'bower_components/',
+        concat: {
+            options: {
+                separator: ';\n',
+            },
+            js: {
+                src: [
+                    '<%= sources %>/angular/angular.min.js',
+                    '<%= sources %>/angular-animate/angular-animate.min.js',
+                    '<%= sources %>/angular-aria/angular-aria.min.js',
+                     '<%= sources %>/angular-material/angular-material.min.js',
+                    '<%= sources %>/angular-messages/angular-messages.min.js'
+                ],
+                dest: 'assets/libs/bundle.js'
+            },
+            css: {
+                src: [
+                    '<%= sources %>/angular-material/angular-material.min.css'
+                ],
+                dest: 'assets/libs/bundle.css'
+            }
+        },
+        sass: {
+            target: {
+                files: {
+                    'assets/css/main.css': 'assets//scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['assets/scss/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                },
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', [ 'concat' ]);
+
+};
